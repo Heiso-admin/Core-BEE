74fa58c7f7a23b3eebecf1cabe67b90f0ad9792b
Meiyi <ant19950601@gmail.com>
2025-11-05 01:08:06 +0800

    feat(auth): implement Microsoft Entra ID login (untested fully)

--- modules/auth/auth.config.ts
+++ modules/auth/auth.config.ts
@@ -1,5 +1,6 @@
 import NextAuth, { CredentialsSignin, type DefaultSession } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
 import { verifyPassword } from '@/lib/hash';
 import { getUser } from './_server/user.service';
 
@@ -43,6 +44,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
   providers: [
+    MicrosoftEntraID({
+      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+      issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID,
+      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+      authorization: {
+        params: {
+          scope: "openid profile email User.Read",
+        },
+      },
+    }),
     Credentials({
       credentials: {
         username: { label: 'Username' },
